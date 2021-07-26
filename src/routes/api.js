6b93ff3d6bba8250e7406093f893cf962571dcbf
Clare Radtke <clare.radtke@outlook.com>
2021-07-26 17:39:27 +1000

    updated inputs to have a min of 0

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,18 +1,15 @@
 const router = require("express").Router();
 const Workout = require("../../models/workout");
 
+// TODO:
+// View the combined weight of multiple exercises from the past seven workouts on the stats page
+// Add exercises to the most recent workout plan.
+// Add new exercises to a new workout plan.
+// View the total duration of each workout from the past seven workouts on the stats page.
+
+// create workout
 router.post("/api/workout", ({ body }, res) => {
   Workout.create(body)
-    .then((dbWorkout) => {
-      res.json(dbWorkout);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.post("/api/workout/bulk", ({ body }, res) => {
-  Workout.insertMany(body)
     .then((dbWorkout) => {
       res.json(dbWorkout);
     })
